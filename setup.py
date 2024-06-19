e80c7dfe7b6967de714449098d0af1b10be1f619
aabdillahi@mail.sfsu.edu <aabdillahi@mail.sfsu.edu>
2024-06-19 09:43:04 -0700

    update

--- setup.py
+++ setup.py
@@ -5,19 +5,6 @@ name, version, and dependencies. '''
 from setuptools import setup, find_packages
 from typing import List
 
-# Reading Requirements 
-HYPEN_E_DOT = '-e'
-
-def get_requirement(file_path:str) -> List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements = f.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements 
-    
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
@@ -28,21 +15,17 @@ AUTHOR_USER_NAME = "Abdoul1996"
 AUTHOR_EMAIL = "aabdillahid@gmail.com"
 
 setup(
-    name= PKG_NAME,
-    version= __version__,
-    author= AUTHOR_USER_NAME,
-    author_email= AUTHOR_EMAIL,
-    description="A python package for connecting with databse (mongodb)",
-    long_description= long_description,
-    long_description_content_type= "textt/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}", 
-    project_urls = {
-        "Bug Tracker" : f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires = get_requirement("./requirements_dev.txt"),
-)
-
-
-
+    )
